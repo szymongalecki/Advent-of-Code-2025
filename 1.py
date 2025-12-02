121e6687ce1d89670df8aa504597bf1b769d0c9a
Szymon Gałecki <szymongalecki@gmail.com>
2025-12-03 00:09:58 +0100

    day 1 part 2

--- 1.py
+++ 1.py
@@ -11,9 +11,9 @@ def rotations(lines: list[str]) -> list[tuple[str, int]]:
 
 def follow(
     start: int,
-    positions: list[int],
     rotations: list[tuple[str, int]],
 ) -> list[int]:
+    positions = []
     for direction, distance in rotations:
         if direction == "R":
             positions.append((start + distance) % 100)
@@ -23,5 +23,33 @@ def follow(
     return positions
 
 
+def clicks(start: int, rotations: list[tuple[str, int]]):
+    cross = 0
+    point = 0
+    for dir, dist in rotations:
+        cross += dist // 100
+        dist %= 100
+        if dir == "R":
+            end = (start + dist) % 100
+            if start > end and start != 0 and end != 0:
+                cross += 1
+        else:
+            end = (start - dist) % 100
+            if start < end and start != 0 and end != 0:
+                cross += 1
+        if end == 0:
+            point += 1
+        start = end
+    return cross + point
+
+
 def part_one():
-    return len([p for p in follow(50, [], rotations(lines())) if p == 0])
+    return len([p for p in follow(50, rotations(lines())) if p == 0])
+
+
+def part_two():
+    return clicks(50, rotations(lines()))
+
+
+print(f"Part one: {part_one()}")
+print(f"Part two: {part_two()}")
