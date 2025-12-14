b82b66265b4953f1fa1f6dd927eee608fd1ac080
Szymon Gałecki <szymongalecki@gmail.com>
2025-12-14 15:17:15 +0100

    day 3 part 2

--- 3.py
+++ 3.py
@@ -17,8 +17,31 @@ def joltage(bank: list[int]) -> int:
     return left * 10 + right
 
 
+def overclock(bank: list[int]) -> int:
+    def batteries(start: int, selected: list[int]) -> list[int]:
+        if len(selected) == 12:
+            return selected
+        s_v = 0
+        s_i = 0
+        for i in range(start, len(bank) - 12 + len(selected) + 1):
+            if bank[i] > s_v:
+                s_v = bank[i]
+                s_i = i
+        return batteries(s_i + 1, selected + [s_v])
+
+    def joltage(batteries: list[int]) -> int:
+        return sum(d * (10**i) for (i, d) in enumerate(reversed(batteries)))
+
+    return joltage(batteries(0, []))
+
+
 def part_one() -> int:
     return sum(joltage(bank) for bank in banks())
 
 
+def part_two() -> int:
+    return sum(overclock(bank) for bank in banks())
+
+
 print(f"Part one: {part_one()}")
+print(f"Part two: {part_two()}")
