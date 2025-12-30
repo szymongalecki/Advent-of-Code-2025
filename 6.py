880ded3e82e0abc7ef36ace8dca1c4e1c231474e
Szymon Gałecki <szymongalecki@gmail.com>
2025-12-30 15:44:31 +0100

    refactor day 6 part 1

--- 6.py
+++ 6.py
@@ -1,31 +1,27 @@
 import sys
+import numpy
+import functools
 
 
 def numbers_operators() -> tuple[list[list[int]], list[str]]:
     lines = [l.strip() for l in open(sys.argv[1], "r").readlines()]
-    numbers = list(map(lambda l: [int(n) for n in l.split()], lines[:-1]))
+    numbers = (
+        numpy.array(list(map(lambda l: [int(n) for n in l.split()], lines[:-1])))
+        .transpose()
+        .tolist()
+    )
     operators = lines[-1].split()
     return numbers, operators
 
 
-def solve(numbers: list[list[int]], operators: list[str]):
-    i_max = len(numbers)
-    j_max = len(numbers[0])
-    res = 0
-    for j in range(j_max):
-        op = operators[j]
-        mul = 1
-        add = 0
-        for i in range(i_max):
-            if op == "*":
-                mul *= numbers[i][j]
-            if op == "+":
-                add += numbers[i][j]
-        if op == "*":
-            res += mul
-        if op == "+":
-            res += add
-    return res
+def solve(numbers: list[list[int]], operators: list[str]) -> int:
+    answers = []
+    for i, o in enumerate(operators):
+        if o == "*":
+            answers.append(functools.reduce(lambda x, y: x * y, numbers[i]))
+        if o == "+":
+            answers.append(functools.reduce(lambda x, y: x + y, numbers[i]))
+    return sum(answers)
 
 
 def part_one() -> int:
